f48ef8d533194c7626e9aa3bd8c3c337777d1c88
hrh858 <jgimenezp99@outlook.com>
2024-07-20 22:46:16 +0900

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        JetBrainsMono: ["JetBrains Mono", "monospace"],
+      },
+      colors: {
+        "hrh-bg": "#23272e",
+        "hrh-dark-blue": "#79b6fb",
+        "hrh-light-blue": "#a0cffc",
+        "hrh-white": "#afbac6",
+        "hrh-muted": "#78838f",
+        "hrh-green": "#6aa760",
+      },
+    },
   },
   plugins: [],
 };
